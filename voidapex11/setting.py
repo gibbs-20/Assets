9ccccd083e4650b6fae377012af2594108c34ed1
voidapex11 <154328367+gibbs-20@users.noreply.github.com>
2024-02-28 16:38:58 +0000

    changed file path for programs

--- voidapex11/setting.py
+++ voidapex11/setting.py
@@ -2,7 +2,7 @@ import json
 class setting:
   def __init__(self, settingName, defaltData=None):
     try:
-      with open("assets/settings.json", "r") as file:
+      with open("Assets/settings.json", "r") as file:
         try:
           tf = json.load(file)
         except:
@@ -14,13 +14,13 @@ class setting:
         except:
           tf[settingName] = defaltData
 
-        with open("assets/settings.json", "w") as file:      
+        with open("Assets/settings.json", "w") as file:      
           json.dump(tf, file)
 
     except:
-      with open("assets/settings.json", "x") as file:
+      with open("Assets/settings.json", "x") as file:
         json.dump(settings, file, indent= 2)
 
-    with open("assets/appData.json", "r") as read_file:
+    with open("Assets/appData.json", "r") as read_file:
       data = json.load(read_file)
       return data[settingName]
